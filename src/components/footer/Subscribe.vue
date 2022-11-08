907675c0fe5d54898be6009e0aa68059e7d9e4ae
Santo Khan <santokhan1999@gmail.com>
2022-11-08 15:23:23 +0600

    Update subscribe

--- src/components/footer/Subscribe.vue
+++ src/components/footer/Subscribe.vue
@@ -1,17 +1,39 @@
+<script>
+export default {
+  data() {
+    return { subscribe: false };
+  },
+  methods: {
+    onSubscribe(e) {
+      e.preventDefault();
+
+      this.subscribe = true;
+    },
+  },
+};
+</script>
 <template>
-  <form class="py-[3rem] max-w-[360px]">
+  <form class="py-[3rem] max-w-[360px]" v-on:submit.prevent="onSubscribe">
+    <div class="text-lg uppercase" v-if="subscribe">
+      Thank you for subscribe
+    </div>
     <div
+      v-else
       class="relative border-b border-stone-400 flex justify-between items-center py-2"
     >
       <input
         class="bg-transparent text-white w-full py-2 outline-none border-none"
         type="text"
         placeholder="Enter Your Email Address"
+        pattern="^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$"
+        required
       />
-      <i
-        class="fab fa-telegram text-xl hover:text-blue-500"
-        aria-hidden="true"
-      ></i>
+      <button type="submit" class="border-none outline-none">
+        <i
+          class="fab fa-telegram text-xl hover:text-blue-500"
+          aria-hidden="true"
+        ></i>
+      </button>
     </div>
   </form>
 </template>
