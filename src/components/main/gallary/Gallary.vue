6e746694a946685f45b344e743e9c7a8370ae787
Santo Khan <santokhan1999@gmail.com>
2022-11-08 22:36:37 +0600

    update product card

--- src/components/main/gallary/Gallary.vue
+++ src/components/main/gallary/Gallary.vue
@@ -6,14 +6,62 @@ import GallaryItem from "./GallaryItem.vue";
     <div
       class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-[2rem]"
     >
-      <GallaryItem></GallaryItem>
-      <GallaryItem></GallaryItem>
-      <GallaryItem></GallaryItem>
-      <GallaryItem></GallaryItem>
-      <GallaryItem></GallaryItem>
-      <GallaryItem></GallaryItem>
-      <GallaryItem></GallaryItem>
-      <GallaryItem></GallaryItem>
+      <GallaryItem
+        src="https://flowbite.com/docs/images/products/apple-watch.png"
+        title="Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport v1"
+        price="499"
+        review="5"
+        link="/watch/rolex"
+      ></GallaryItem>
+      <GallaryItem
+        src="https://m.media-amazon.com/images/I/61-NICYleSL._AC_UL320_.jpg"
+        title="Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport v2"
+        price="399"
+        review="5"
+        link="/watch/rolex"
+      ></GallaryItem>
+      <GallaryItem
+        src="https://flowbite.com/docs/images/products/apple-watch.png"
+        title="Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport v3"
+        price="699"
+        review="5"
+        link="/watch/rolex"
+      ></GallaryItem>
+      <GallaryItem
+        src="https://m.media-amazon.com/images/I/61-NICYleSL._AC_UL320_.jpg"
+        title="Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport v4"
+        price="999"
+        review="5"
+        link="/watch/rolex"
+      ></GallaryItem>
+      <GallaryItem
+        src="https://flowbite.com/docs/images/products/apple-watch.png"
+        title="Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport v5"
+        price="499"
+        review="5"
+        link="/watch/rolex"
+      ></GallaryItem>
+      <GallaryItem
+        src="https://m.media-amazon.com/images/I/61-NICYleSL._AC_UL320_.jpg"
+        title="Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport v6"
+        price="199"
+        review="3"
+        link="/watch/rolex"
+      ></GallaryItem>
+      <GallaryItem
+        src="https://flowbite.com/docs/images/products/apple-watch.png"
+        title="Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport v7"
+        price="299"
+        review="4"
+        link="/watch/rolex"
+      ></GallaryItem>
+      <GallaryItem
+        src="https://m.media-amazon.com/images/I/61-NICYleSL._AC_UL320_.jpg"
+        title="Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport v8"
+        price="599"
+        review="5"
+        link="/watch/rolex"
+      ></GallaryItem>
     </div>
   </div>
 </template>
--- src/components/main/gallary/GallaryItem.vue
+++ src/components/main/gallary/GallaryItem.vue
@@ -1,36 +1,43 @@
+<script>
+export default {
+  data() {
+    return { r: 0, reviewArr: [1, 2, 3, 4, 5] };
+  },
+  props: ["src", "title", "price", "review", "link"],
+};
+</script>
 <template>
   <div className="w-full max-w-sm bg-white rounded-lg shadow-md">
     <a href="#">
       <img
-        className="p-8 rounded-t-lg"
-        src="https://flowbite.com/docs/images/products/apple-watch.png"
+        className="p-8 rounded-t-lg w-full h-60 object-contain"
+        :src="src"
         alt="product image"
       />
     </a>
     <div className="px-5 pb-5">
-      <a href="#">
+      <a :href="link">
         <h5 className="text-xl font-semibold tracking-tight">
-          Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
+          {{ title }}
         </h5>
       </a>
       <div className="flex items-center mt-2.5 mb-5">
-        <i class="fa fa-star text-yellow-400" aria-hidden="true"></i>
-        <i class="fa fa-star text-yellow-400" aria-hidden="true"></i>
-        <i class="fa fa-star text-yellow-400" aria-hidden="true"></i>
-        <i class="fa fa-star text-yellow-400" aria-hidden="true"></i>
-        <i class="fa fa-star text-yellow-400" aria-hidden="true"></i>
+        <i
+          v-for="item in reviewArr"
+          v-if="r < review"
+          class="fa fa-star text-yellow-400"
+          aria-hidden="true"
+        ></i>
         <span
           className="bg-blue-100 text-blue-800 text-xs font-semibold mx-2 px-2.5 py-0.5 rounded"
         >
-          5.0
+          {{ review }}.0
         </span>
       </div>
       <div className="flex justify-between items-center">
-        <span className="text-3xl font-bold text-gray-900 dark:text-white">
-          $599
-        </span>
+        <span className="text-3xl font-bold text-gray-900"> ${{ price }} </span>
         <a
-          href="#"
+          :href="link"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
           >Add to cart</a
         >
